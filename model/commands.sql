7e55a92bd4df44c91fa9647c263a6ae130eec6ca
imazlin <mazlin.ismael@gmail.com>
2024-04-22 12:53:23 +0200

    initDatabaseEnd

--- model/commands.sql
+++ model/commands.sql
@@ -7,3 +7,18 @@ table categories => id PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, categorie VARC
 table comments => id PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL , user (foreign key), content VARCHAR(5000), post (foreign key), date datetime
 
 table post_categories => id post(foreign key), id categorie (foreign key)
+
+
+CREATE TABLE `users` (`id` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, username VARCHAR(25) NOT NULL, password VARCHAR(40) NOT NULL, 
+email VARCHAR(320) NOT NULL UNIQUE);
+
+CREATE TABLE `posts` (`id` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, title VARCHAR(100), 
+content VARCHAR(10000), user_id INTEGER, likes int, dislikes int, date datetime DEFAULT current_timestamp, FOREIGN KEY(user_id) REFERENCES users(id));
+
+CREATE TABLE `categories` (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, category VARCHAR(30));
+
+CREATE TABLE `comments` (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, user_id INTEGER, content VARCHAR(5000), post_id INTEGER, 
+date datetime DEFAULT current_timestamp, FOREIGN KEY(user_id) REFERENCES users(id), FOREIGN KEY(post_id) REFERENCES posts(id));
+
+CREATE TABLE `post_categories` (post_id INTEGER, category_id, FOREIGN KEY(post_id) REFERENCES posts(id), FOREIGN KEY(category_id) REFERENCES categories(id), 
+PRIMARY KEY(post_id, category_id));
