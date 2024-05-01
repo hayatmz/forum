6f7b057e197e65c7df38df2ae88f5d7cde642b38
bdesouza <bryan.desou@gmail.com>
2024-05-01 15:35:52 +0200

    posts order by date in category

--- model/category.go
+++ model/category.go
@@ -1,32 +1,33 @@
 package model
 
 type Post struct {
-    ID        int
-    Title     string
-    Content   string
-    UserID    int
+	ID      int
+	Title   string
+	Content string
+	UserID  int
 }
 
-
 func GetPostsByCategory(category string) ([]Post, error) {
-    query :=   `SELECT posts.id, posts.title, posts.content, posts.user_id
+	query := `SELECT posts.id, posts.title, posts.content, posts.user_id
                 FROM posts
                 INNER JOIN post_categories ON posts.id = post_categories.post_id
                 INNER JOIN categories ON post_categories.category_id = categories.id
-                WHERE categories.category = ? `
-    rows, err := db.Query(query, category)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
+                WHERE categories.category = ? 
+                ORDER BY posts.date DESC`
+	rows, err := db.Query(query, category)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-    var posts []Post
-    for rows.Next() {
-        var post Post
-        if err := rows.Scan(&post.ID, &post.Title, &post.Content, &post.UserID); err != nil {
-            return nil, err
-        }
-        posts = append(posts, post)
-    }
-    return posts, nil
+	var posts []Post
+	for rows.Next() {
+		var post Post
+		err := rows.Scan(&post.ID, &post.Title, &post.Content, &post.UserID)
+        if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
 }
