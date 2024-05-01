ebbda0b3d27fc0920fe69ca4acce88796a49218d
imazlin <mazlin.ismael@gmail.com>
2024-05-01 20:33:46 +0200

    resolution conflicts between v2LoadTemplates and v2CommenstLikes

--- controller/handler.go
+++ controller/handler.go
@@ -8,9 +8,14 @@ func handlers(mux *http.ServeMux) {
 	mux.HandleFunc("/", rootPage)
 	mux.HandleFunc("/registerPage", registerPage)
 	mux.HandleFunc("/registerForm", registerForm)
-	mux.HandleFunc("/postPage", postPage)
-	mux.HandleFunc("/postForm", postForm)
+
 	mux.HandleFunc("/loginPage", loginPage)
 	mux.HandleFunc("/loginForm", loginForm)
 	mux.HandleFunc("/category", categoryPage)
+	
+	mux.HandleFunc("/postPage", postPage)
+	mux.HandleFunc("/postForm", postForm)
+	
+	mux.HandleFunc("/postLoadPage", postLoadPage)
+	mux.HandleFunc("/postLoadForm", postLoadForm)
 }
