90e924dca4f6f763018bfee12b875c380ef167ed
hmazlin <hayatmazlin@outlook.fr>
2024-05-06 22:22:21 +0200

    header co et pas co done

---
+++ view/static/HeaderScriptHayat.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".fa").click(function () {
+        $(".HeaderLinksResponsive").fadeToggle(500);
+    });
+
+    $(".HeaderLinksResponsive li").click(function () {
+        $(".HeaderLinksResponsive").fadeToggle(500);
+    });
+});
