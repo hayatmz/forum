fde1fcce4c7be2cba42df0dfe8913500adf16a04
bdesouza <bryan.desou@gmail.com>
2024-05-07 16:58:14 +0200

    uuid start

--- model/user.go
+++ model/user.go
@@ -1,64 +1,68 @@
 package model
 
 import (
-	"database/sql"
-	"errors"
+    "database/sql"
+    "errors"
+    "github.com/gofrs/uuid"
 )
-
 
 // Checks if the user is already in the database
 func VerifyUserLogin(email, password string) error {
-	var storedPassword string
-	err := db.QueryRow("SELECT password FROM users WHERE email = ?", email).Scan(&storedPassword)
+    var storedPassword string
+    err := db.QueryRow("SELECT password FROM users WHERE email = ?", email).Scan(&storedPassword)
 
-	if err == sql.ErrNoRows {
-		return errors.New("no account associated for this email")
-	} else if err != nil {
-		return err
-	}
+    if err == sql.ErrNoRows {
+        return errors.New("no account associated for this email")
+    } else if err != nil {
+        return err
+    }
 
-	if storedPassword != password {
-		return errors.New("bad password for this account")
-	}
+    if storedPassword != password {
+        return errors.New("bad password for this account")
+    }
 
-	return nil
+    return nil
 }
 
-
 func VerifyUserRegister(email, username, password string) error {
-	
-	err := chekingUserInDB(email, username, password)
-	if err != nil {
-		return err
-	}
+    userID, err := uuid.NewV4()
+    if err != nil {
+        return err
+    }
+    userIDString := userID.String()
 
-	queryUserRegister := "INSERT INTO users(email, username, password) VALUES(?,?,?);"
-	_, err = execQuery(queryUserRegister, email, username, password)
-	if err != nil {
-		return err
-	}
-	return nil
+    err = chekingUserInDB(email, username, password)
+    if err != nil {
+        return err
+    }
+
+    queryUserRegister := "INSERT INTO users(id, email, username, password) VALUES(?,?,?,?);"
+    _, err = execQuery(queryUserRegister, userIDString, email, username, password)
+    if err != nil {
+        return err
+    }
+    return nil
 }
 
 func chekingUserInDB(email, username, password string) error {
-	var isFieldExisting bool
-	
-	queryEmail := "SELECT EXISTS(SELECT 1 FROM users WHERE email=?)"
-	err := db.QueryRow(queryEmail, email).Scan(&isFieldExisting)
-	if err != nil {
-		return err
-	}
-	if isFieldExisting {
-		return errors.New("email already taken")
-	}
+    var isFieldExisting bool
+    
+    queryEmail := "SELECT EXISTS(SELECT 1 FROM users WHERE email=?)"
+    err := db.QueryRow(queryEmail, email).Scan(&isFieldExisting)
+    if err != nil {
+        return err
+    }
+    if isFieldExisting {
+        return errors.New("email already taken")
+    }
 
-	queryUserName := "SELECT EXISTS(SELECT 1 FROM users WHERE email=?)"
-	err = db.QueryRow(queryUserName, password).Scan(&isFieldExisting)
-	if err != nil {
-		return err
-	}
-	if isFieldExisting {
-		return errors.New("username already taken")
-	}
-	return nil
+    queryUserName := "SELECT EXISTS(SELECT 1 FROM users WHERE email=?)"
+    err = db.QueryRow(queryUserName, password).Scan(&isFieldExisting)
+    if err != nil {
+        return err
+    }
+    if isFieldExisting {
+        return errors.New("username already taken")
+    }
+    return nil
 }
