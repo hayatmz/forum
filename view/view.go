628d0229d33c990f0c0fe00ab16b3f1fe21b8223
imazlin <mazlin.ismael@gmail.com>
2024-05-09 19:34:08 +0200

    if templates doesn't load redirect http.Error(500)

--- view/view.go
+++ view/view.go
@@ -11,13 +11,17 @@ func init() {
 	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	tmpl = template.Must(template.ParseGlob("view/templates/*"))
 	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("view/static"))))
-	// http.Handle("/visualizer/static/", http.StripPrefix("/visualizer/static/", http.FileServer(http.Dir("visualizer/static"))))
+	http.Handle("/visualizer/static/", http.StripPrefix("/visualizer/static/", http.FileServer(http.Dir("visualizer/static"))))
 }
 
 func ExecTemplate(w http.ResponseWriter, tpl string, data any) {
 	err := tmpl.ExecuteTemplate(w, tpl, data)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		tmpl.ExecuteTemplate(w, "error", http.StatusInternalServerError)
+		err := tmpl.ExecuteTemplate(w, "error", http.StatusInternalServerError)
+		if err != nil {
+			http.Error(w, "Internal Servor Error", http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 	}
 }
