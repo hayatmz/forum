1817a911c1805d328faade5db197b3e6fa717d7a
imazlin <mazlin.ismael@gmail.com>
2024-05-10 08:58:11 +0200

    add static to view

--- view/view.go
+++ view/view.go
@@ -9,19 +9,18 @@ var tmpl *template.Template
 
 func init() {
 	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/view/static/", http.StripPrefix("/view/static/", http.FileServer(http.Dir("view/static"))))
 	tmpl = template.Must(template.ParseGlob("view/templates/*"))
 	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("view/static"))))
-	http.Handle("/visualizer/static/", http.StripPrefix("/visualizer/static/", http.FileServer(http.Dir("visualizer/static"))))
 }
 
 func ExecTemplate(w http.ResponseWriter, tpl string, data any) {
 	err := tmpl.ExecuteTemplate(w, tpl, data)
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		err := tmpl.ExecuteTemplate(w, "error", http.StatusInternalServerError)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
 		}
 	}
 }
