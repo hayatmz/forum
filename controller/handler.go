1d67f0746646a74d6ee2c3f17790be2a9d9fdd8d
imazlin <mazlin.ismael@gmail.com>
2024-06-04 04:37:53 +0200

    handler regrouping

--- controller/handler.go
+++ controller/handler.go
@@ -7,28 +7,21 @@ import (
 )
 
 func handlers(mux *http.ServeMux) {
-	mux.Handle("/", isConnected(http.HandlerFunc(rootPage)))
-	
+	mux.Handle("/", isConnected(http.HandlerFunc(rootPage)))	
 	mux.Handle("/registerPage", isConnected(http.HandlerFunc(registerPage)))
 	mux.Handle("/registerForm", checkFormValues(http.HandlerFunc(registerForm)))
 	mux.Handle("/loginPage", isConnected(http.HandlerFunc(loginPage)))
-	mux.Handle("/loginForm", isConnected(http.HandlerFunc(loginForm)))
-	
+	mux.Handle("/loginForm", isConnected(http.HandlerFunc(loginForm)))	
 	mux.Handle("/pageNewPost", isConnected(http.HandlerFunc(pageNewPost)))
 	mux.Handle("/formNewPost", checkValidSession(checkFormValues(http.HandlerFunc(formNewPost))))
-	
 	mux.Handle("/postLoadForm", isConnected(http.HandlerFunc(postLoadForm)))
 	mux.Handle("/comForm", checkValidSession(checkFormValues(http.HandlerFunc(comForm))))
-
 	mux.Handle("/like-comment", checkValidSession(http.HandlerFunc(likeCommentForm)))
 	mux.Handle("/dislike-comment", checkValidSession(http.HandlerFunc(dislikeCommentForm)))
-
-
 	mux.Handle("/likeForm", checkValidSession(http.HandlerFunc(likeForm)))
 	mux.Handle("/dislikeForm", checkValidSession(http.HandlerFunc(dislikeForm)))
 	mux.Handle("/postsByLikes", checkValidSession(http.HandlerFunc(postsByLikes)))
 	mux.Handle("/postsByUser", checkValidSession(http.HandlerFunc(postsByUser)))
-
 	mux.Handle("/category", isConnected(http.HandlerFunc(categoryPage)))
 }
 
